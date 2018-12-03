dfa1294dae72c4c320447aa16851af71d0ac7879
danilocutrim <danilocutrim.dc@gmail.com>
2018-12-03 21:05:49 -0200

    herança de templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Olá mundo"
+    user = {'username': 'Danilo'}
+    posts = [
+        {
+            'author': {'username': 'Rosi'},
+            'body': 'Semana de p2 na ufabc'
+        },
+        {
+            'author': {'username': 'Danilo'},
+            'body': 'Final de ano'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
