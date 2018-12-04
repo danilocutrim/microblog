f3c7aff9fea113d6ab525faf9166afb1b39c088a
danilocutrim <danilocutrim.dc@gmail.com>
2018-12-04 12:50:26 -0200

    flask-login

--- app/models.py
+++ app/models.py
@@ -1,13 +1,23 @@
 from app import db
 from datetime import datetime
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)   
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,3 +27,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+#O carregador do usuário é registrado no Flask-Login com o @login.user_loaderdecorador. O idque o Flask-Login passa para a função como um argumento será uma string, então os bancos de dados que usam IDs numéricos precisam converter a string em inteiro como você vê acima.
+
